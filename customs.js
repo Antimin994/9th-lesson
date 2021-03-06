c148f8af75092c4fdcc8d76ae795a7361ad732fe
Antimin994 <anton.antimijchuk@gmail.com>
2021-03-07 00:36:12 +0200

    second commit

--- customs.js
+++ customs.js
@@ -1,5 +1,5 @@
 var obj = {'Коля':'1000', 'Вася':'500', 'Петя':'200'};
-document.write('Коля зарабатывает ' + obj.Коля + '<br>Петя зарабатывает ' + obj.Петя + '<br>');
+document.write('Коля зарабатывает ' + obj['Коля'] + '<br>Петя зарабатывает ' + obj['Петя'] + '<br>');
 
 
 
@@ -14,11 +14,10 @@ document.write(arr['ru'][0] + '<br>');
 var arr11 = {
 'ru':['голубой', 'красный', 'зеленый'],
     get newColor() {
-     return arr11['ru']   
+    return this['ru'];   
     },
     set newColor(value) {
-    this.key = value.split(' ')    
+    this.key.push(value); 
     }
 };
 document.write(arr11.newColor);
-
